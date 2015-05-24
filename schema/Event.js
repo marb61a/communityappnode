8d99000912bae3d8a8a43e2bf52b9fd495db97bf
marb <marb61a@gmail.com>
2015-05-24 08:56:44 +0000

    added event adding

--- schema/Event.js
+++ schema/Event.js
@@ -14,7 +14,7 @@ exports = module.exports = function(app, mongoose){
         eventSchema.index({ name: 1 });
         eventSchema.index({ username: 1 });
         eventSchema.index({ date: 1 });
-        eventSchema.index({ venu: 1 });
+        eventSchema.index({ venue: 1 });
         eventSchema.index({ startTime: 1 });
         eventSchema.index({ endTime: 1 });
         eventSchema.index({ search: 1 });
